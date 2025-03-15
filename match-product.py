725da39ad7a06c99b7ca3d6d9423f52cd293309d
Oleh <olegprogir@gmail.com>
2025-03-15 17:45:17 +0100

    added some

--- match-product.py
+++ match-product.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from spec_checker import get_all_methods, find_best_choice
+import json
 
 app = Flask(__name__)
 
@@ -36,5 +36,39 @@ def get_feature(query: str):
     #       'monitor' : ...
     #     }
     # }
+
+    return None
+
+
+def db_search(item: dict):
+    # returns search on basic specs
+
+    with open('db.json', 'r') as file:
+        lst = json.load(file)
+
+    print(lst)
+    matching_items = []
+    for db_item in lst:
+        if all(item.get(key) == db_item.get(key) for key in item):
+            matching_items.append(db_item)
+
+    return matching_items
+
+
+with open('sample-item.json', 'r') as file:
+    item = json.load(file)
+
+matching_items = db_search(item)
+print(json.dumps(matching_items, indent=4))
+
+
+def check_additional_specs(query, item):
+    # returns True/False if the additional specs match the item
+
+    return None
+
+
+def spec_sheet_checker(query: dict):
+    # performs additional checks on DBSearch
 
     return None
